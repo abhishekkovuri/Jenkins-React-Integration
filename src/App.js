83e1f94678e0c105c12f5042acd6eb4ff2ff232b
Abhishek Kovuri <abhishekkovuri@Abhisheks-MacBook-Pro.local>
2022-02-13 23:04:52 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Jenkins Integration with My App
+      </header>
+    </div>
+  );
+}
+
+export default App;
